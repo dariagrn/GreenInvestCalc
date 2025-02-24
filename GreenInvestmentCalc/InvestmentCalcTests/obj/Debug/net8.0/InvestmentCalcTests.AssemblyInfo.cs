859b4ecdc9163e806ec9e8aec237c04f859e3971
dariagrn <daria.green20@gmail.com>
2025-02-24 16:59:20 -0700

    update variable name

--- GreenInvestmentCalc/InvestmentCalcTests/obj/Debug/net8.0/InvestmentCalcTests.AssemblyInfo.cs
+++ GreenInvestmentCalc/InvestmentCalcTests/obj/Debug/net8.0/InvestmentCalcTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvestmentCalcTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7ee6e14e816e5f6e325d30b8a40494c35f56b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb3a7132b7022d329aa13d80e4edd53f781544b")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvestmentCalcTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvestmentCalcTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
