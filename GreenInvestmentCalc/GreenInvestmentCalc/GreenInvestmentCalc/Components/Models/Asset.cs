a1529c489e4bee3d443c2f9f7b902b657a77bb06
dariagrn <daria.green20@gmail.com>
2025-03-24 14:56:19 -0600

    InvestmentCalc with Assets lists and details

---
+++ GreenInvestmentCalc/GreenInvestmentCalc/GreenInvestmentCalc/Components/Models/Asset.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreenInvestmentCalc.Components.Models
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Asset Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Entry Date is required.")]
+        public DateTime EntryDate { get; set; }
+
+        [Required(ErrorMessage = "Exit Date is required.")]
+        public DateTime ExitDate { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Starting Value must be a positive number.")]
+        public double StartingValue { get; set; }
+
+
+        [Range(0, double.MaxValue, ErrorMessage = "Salvage Value must be a positive number.")]
+        public double SalvageValue { get; set; }
+    }
+}
